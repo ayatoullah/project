d1e08af67237e493e196c153ad5559c38eab691e
ayatoullah <ayaramzy2014@gmail.com>
2018-12-06 19:47:34 +0200

    fix all bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
 import { FormsModule } from '@angular/forms';
 import {ModalModule} from 'ngx-bootstrap'
 import { Try2Component } from './try2/try2.component';
+import { InternationalPhoneNumberModule } from 'ngx-international-phone-number';
+
 // import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader';
 
 
@@ -35,7 +37,7 @@ import { Try2Component } from './try2/try2.component';
     BsDropdownModule.forRoot(),
     NgxIntlTelInputModule,
     // ComponentLoaderFactory,
-    
+    InternationalPhoneNumberModule,
     FormsModule,
     ModalModule.forRoot()
     
--- src/app/verifymobile/verifymobile.component.ts
+++ src/app/verifymobile/verifymobile.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { ComponentLoaderFactory } from 'ngx-bootstrap/component-loader';
 
+
 @Component({
   selector: 'app-verifymobile',
   templateUrl: './verifymobile.component.html',
   styleUrls: ['./verifymobile.component.css'],
   providers: [ComponentLoaderFactory]
 })
+
 //  @NgModule()
 export class VerifymobileComponent implements OnInit {
-
+  phone_number:number;
   constructor() { }
 
   ngOnInit() {
